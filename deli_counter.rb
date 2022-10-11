aa54b769471b84f06d347996ffb56adb78d9d0f0
SiobhanMcCaffrey <siobhanamccaffrey@gmail.com>
2022-10-11 12:26:12 -0400

    commit tree 6d250b3728f326e6ca2dd4a979e7097a2cbaad3d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+katz_deli = []
+
+def line(queue)
+    if queue.length == 0
+    puts "The line is currently empty."
+
+    else
+    current_line = "The line is currently:"
+    queue.each.with_index(1) do |person, index|
+        current_line << " #{index}. #{person}"
+
+    end
+    
+    puts current_line
+    end
+
+end
+
+def take_a_number(queue, name)
+    queue << name
+    puts "Welcome, #{name}. You are number #{queue.length} in line."
+end
+
+def now_serving(queue)
+    if queue.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{queue.first}."
+        queue.shift
+    end
+
+end
+
